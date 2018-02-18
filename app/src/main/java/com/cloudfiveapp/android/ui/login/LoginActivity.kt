ce2c01d6f968e3a916df75b27ffc46e069ce5102
Alex Petitjean <alex@tenforwardconsulting.com>
2018-02-18 10:10:17 -0600

    Add login screen with cloud animations

---
+++ app/src/main/java/com/cloudfiveapp/android/ui/login/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.cloudfiveapp.android.ui.login
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.cloudfiveapp.android.R
+import com.cloudfiveapp.android.application.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : BaseActivity() {
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, LoginActivity::class.java)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        lifecycle.addObserver(CloudAnimator(loginParentView, layoutInflater))
+    }
+}
--- app/src/main/java/com/cloudfiveapp/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/cloudfiveapp/android/ui/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.cloudfiveapp.android.ui.main
 import android.os.Bundle
 import com.cloudfiveapp.android.R
 import com.cloudfiveapp.android.application.BaseActivity
+import com.cloudfiveapp.android.ui.login.LoginActivity
 import com.cloudfiveapp.android.ui.releaseslist.ReleasesListActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -14,10 +16,13 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onResume() {
-        super.onResume()
-        startActivity(ReleasesListActivity.newIntent(this))
+        mainLoginButton.setOnClickListener {
+            startActivity(LoginActivity.newIntent(this))
+        }
+
+        mainReleasesButton.setOnClickListener {
+            startActivity(ReleasesListActivity.newIntent(this))
+        }
     }
 }
