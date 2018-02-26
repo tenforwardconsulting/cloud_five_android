c8ead0ceccb4fb22b7ea58307c36520dc11dff44
Alex Petitjean <alex@tenforwardconsulting.com>
2018-02-26 12:40:31 -0600

    Add logging interceptor and network headers

--- app/src/main/java/com/cloudfiveapp/android/application/di/NetworkModule.kt
+++ app/src/main/java/com/cloudfiveapp/android/application/di/NetworkModule.kt
@@ -9,7 +9,9 @@ import dagger.Module
 import dagger.Provides
 import io.reactivex.schedulers.Schedulers
 import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -41,13 +43,45 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun providesOkHttpClient(cache: Cache): OkHttpClient {
-        val client = OkHttpClient.Builder().cache(cache)
+    fun providesOkHttpClient(cache: Cache,
+                             loggingInterceptor: HttpLoggingInterceptor,
+                             headerInterceptor: Interceptor): OkHttpClient {
+        val client = OkHttpClient.Builder()
+                .cache(cache)
+                .addInterceptor(loggingInterceptor)
         if (BuildConfig.DEBUG) {
             client.addNetworkInterceptor(StethoInterceptor())
         }
         return client.build()
     }
+
+    @Provides
+    @Singleton
+    fun providesHeadersInterceptor(@Named("apiVersion") apiVersion: String): Interceptor {
+        return Interceptor { chain: Interceptor.Chain ->
+            val request = chain.request().newBuilder()
+                    .addHeader("Accept", apiVersion)
+                    .addHeader("X-Cloud-Five-Platform", "android")
+                    .addHeader("X-Cloud-Five-Device-Name", android.os.Build.MODEL)
+                    .addHeader("X-Cloud-Five-Device-System-Version", android.os.Build.VERSION.RELEASE)
+                    .addHeader("X-Cloud-Five-App-Version", BuildConfig.VERSION_NAME)
+                    .addHeader("X-Cloud-Five-App-Build-Number", BuildConfig.VERSION_CODE.toString())
+                    .addHeader("X-Cloud-Five-Git-Commit", BuildConfig.GIT_HASH)
+                    .build()
+            chain.proceed(request)
+        }
+    }
+
+    @Provides
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    @Provides
+    @Named("apiVersion")
+    fun providesApiVersion() = "application/vnd.cloudfive.v1"
 
     @Provides
     @Singleton
