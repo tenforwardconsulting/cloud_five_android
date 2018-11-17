f0b5584c27457b1070b38ae8128034745236661f
Alex Petitjean <alex@tenforwardconsulting.com>
2018-11-17 09:56:48 -0600

    Fake log in
    Adds some transitions for releases list too.

--- app/src/main/java/com/cloudfiveapp/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/cloudfiveapp/android/ui/main/MainActivity.kt
@@ -2,15 +2,28 @@ package com.cloudfiveapp.android.ui.main
 
 import android.app.DownloadManager
 import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.cloudfiveapp.android.R
 import com.cloudfiveapp.android.application.BaseActivity
+import com.cloudfiveapp.android.ui.login.LoginActivity
 import com.cloudfiveapp.android.ui.releaseslist.ReleaseDownloadBroadcastReceiver
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    companion object {
+        var loggedIn = false
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
 
     private val downloadManager: DownloadManager by lazy {
         getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
@@ -20,9 +33,26 @@ class MainActivity : BaseActivity() {
         ReleaseDownloadBroadcastReceiver(mainRootView, this, downloadManager)
     }
 
+    private val navController by lazy { findNavController(R.id.navHostFragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!loggedIn) {
+            startActivity(LoginActivity.newIntent(this))
+            finish()
+        } else {
+            val navHost = NavHostFragment.create(R.navigation.nav_graph)
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.navHostFragment, navHost)
+                    .setPrimaryNavigationFragment(navHost)
+                    .runOnCommit {
+                        val appBarConfiguration = AppBarConfiguration(navController.graph)
+                        mainToolbar.setupWithNavController(navController, appBarConfiguration)
+                    }
+                    .commit()
+        }
     }
 
     override fun onResume() {
@@ -35,5 +65,5 @@ class MainActivity : BaseActivity() {
         unregisterReceiver(releaseDownloadBroadcastReceiver)
     }
 
-    override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
