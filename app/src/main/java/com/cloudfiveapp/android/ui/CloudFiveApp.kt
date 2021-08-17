f56ff87654c3bc2b640e4718ad860a76bfe38ace
Alex Petitjean <alex@tenforwardconsulting.com>
2021-08-17 16:31:36 -0500

    Add stubbed navigation between login and showcase

--- app/src/main/java/com/cloudfiveapp/android/ui/CloudFiveApp.kt
+++ app/src/main/java/com/cloudfiveapp/android/ui/CloudFiveApp.kt
@@ -2,10 +2,19 @@ package com.cloudfiveapp.android.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cloudfiveapp.android.ui.login.LoginScreen
 import com.cloudfiveapp.android.ui.stubs.showcase.ThemeShowcase
 
 @Preview(showSystemUi = true)
 @Composable
 fun CloudFiveApp() {
-    ThemeShowcase()
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController = navController) }
+        composable("showcase") { ThemeShowcase() }
+    }
 }
--- app/src/main/java/com/cloudfiveapp/android/ui/login/LoginScreen.kt
+++ app/src/main/java/com/cloudfiveapp/android/ui/login/LoginScreen.kt
@@ -1,20 +1,31 @@
 package com.cloudfiveapp.android.ui.login
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Preview
 @Composable
-fun LoginScreen() {
+fun LoginScreen(
+    navController: NavController = rememberNavController(),
+) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Box(contentAlignment = Alignment.Center) {
-            Text("Hello Login")
+            Column {
+                Text("Hello Login")
+                Button(onClick = { navController.navigate("showcase") }) {
+                    Text("Login")
+                }
+            }
         }
     }
 }
