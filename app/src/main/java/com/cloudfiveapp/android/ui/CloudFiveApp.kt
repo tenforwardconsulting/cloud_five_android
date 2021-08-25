27d0ebe7e9ad3fb247bb42db18aaf40eb157f878
Alex Petitjean <alex@tenforwardconsulting.com>
2021-08-25 10:48:43 -0500

    Create poorly styled but functional login form

--- app/src/main/java/com/cloudfiveapp/android/ui/CloudFiveApp.kt
+++ app/src/main/java/com/cloudfiveapp/android/ui/CloudFiveApp.kt
@@ -2,10 +2,12 @@ package com.cloudfiveapp.android.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.cloudfiveapp.android.ui.login.LoginScreen
+import com.cloudfiveapp.android.ui.login.LoginViewModel
 import com.cloudfiveapp.android.ui.stubs.showcase.ThemeShowcase
 
 @Preview(showSystemUi = true)
@@ -14,7 +16,14 @@ fun CloudFiveApp() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "login") {
-        composable("login") { LoginScreen(navController = navController) }
+        composable("login") {
+            val viewModel: LoginViewModel = hiltViewModel()
+            LoginScreen(onLoginClick = { email, password ->
+                viewModel.signIn(email = email, password = password) {
+                    navController.navigate("showcase")
+                }
+            })
+        }
         composable("showcase") { ThemeShowcase() }
     }
 }
--- app/src/main/java/com/cloudfiveapp/android/ui/login/LoginScreen.kt
+++ app/src/main/java/com/cloudfiveapp/android/ui/login/LoginScreen.kt
@@ -6,33 +6,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 
 @Preview
 @Composable
 fun LoginScreen(
-    // TODO: Remove this default parameter value with @PreviewParameter somehow?
-    navController: NavController = rememberNavController(),
-    viewModel: LoginViewModel = hiltViewModel(),
+    onLoginClick: (email: String, password: String) -> Unit = { _, _ -> },
 ) {
     Surface(modifier = Modifier.fillMaxSize()) {
         Box(contentAlignment = Alignment.Center) {
             Column {
-                Text("Hello Login")
-                Button(
-                    onClick = {
-                        viewModel.signIn(email = "alex@10fw.net", password = "password") {
-                            navController.navigate("showcase")
-                        }
-                    }
-                ) {
+                var email by remember { mutableStateOf("") }
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") },
+                    placeholder = { Text("Email") },
+                )
+
+                var password by remember { mutableStateOf("") }
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") },
+                    placeholder = { Text("Password") },
+                )
+
+                Button(onClick = { onLoginClick(email, password) }) {
                     Text("Login")
                 }
             }
